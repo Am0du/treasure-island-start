7a399edb45d2265cdd4d911d6b8e3bd6c3c7c429
Amodu David <104693505+Am0du@users.noreply.github.com>
2023-02-21 20:41:22 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,45 @@
+print('''
+*******************************************************************************
+          |                   |                  |                     |
+ _________|________________.=""_;=.______________|_____________________|_______
+|                   |  ,-"_,=""     `"=.|                  |
+|___________________|__"=._o`"-._        `"=.______________|___________________
+          |                `"=._o`"=._      _`"=._                     |
+ _________|_____________________:=._o "=._."_.-="'"=.__________________|_______
+|                   |    __.--" , ; `"=._o." ,-"""-._ ".   |
+|___________________|_._"  ,. .` ` `` ,  `"-._"-._   ". '__|___________________
+          |           |o`"=._` , "` `; .". ,  "-._"-._; ;              |
+ _________|___________| ;`-.o`"=._; ." ` '`."\` . "-._ /_______________|_______
+|                   | |o;    `"-.o`"=._``  '` " ,__.--o;   |
+|___________________|_| ;     (#) `-.o `"=.`_.--"_o.-; ;___|___________________
+____/______/______/___|o;._    "      `".o|o_.--"    ;o;____/______/______/____
+/______/______/______/_"=._o--._        ; | ;        ; ;/______/______/______/_
+____/______/______/______/__"=._o--._   ;o|o;     _._;o;____/______/______/____
+/______/______/______/______/____"=._o._; | ;_.--"o.--"_/______/______/______/_
+____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
+/______/______/______/______/______/______/______/______/______/______/_____ /
+*******************************************************************************
+''')
+print("Welcome to Treasure Island.")
+print("Your mission is to find the treasure.") 
+
+#https://www.draw.io/?lightbox=1&highlight=0000ff&edit=_blank&layers=1&nav=1&title=Treasure%20Island%20Conditional.drawio#Uhttps%3A%2F%2Fdrive.google.com%2Fuc%3Fid%3D1oDe4ehjWZipYRsVfeAx2HyB7LCQ8_Fvi%26export%3Ddownload
+
+#Write your code below this line 👇
+start = input("Type 'Left' or 'Right' to begin " )
+start.lower()
+
+if start == "left":
+  start = input("There's a stream in front of you, Type 'wait' to wait for a boat(This could take an hour) or 'Swim' ")
+  if start == "wait":
+    start = input("There's a door in front of you, Type 'Red' to enter the Red door /n 'Blue' to enter the Blue door and \n 'Yellow' to enter the yellow door ")
+    if start == "yellow":
+      print("YOU WIN, Grab all your Treasure.")
+    elif start == "red":
+      print("GAME OVER, Burned by fire \n Reload to start over.")
+    elif start == "blue":
+      print("GAME OVER, You being eaten by beasts.  \n Reload to start over")
+  else:
+    print("GAME OVER!!!, you were attacked by a Croc \n Reload to start over.")
+else:
+  print("Yaks, You fallen into a pit. GAME OVER \n Reload to start over")
